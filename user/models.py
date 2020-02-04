4d15f9b0a611943160582c50a5e861f28197b2e8
Daria <daria@example.com>
2020-02-04 13:04:11 +0200

    Profile model - gettext added

--- user/models.py
+++ user/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext as _
 
 
 class Profile(models.Model):
     class Role(models.IntegerChoices):
-        ADMIN = 0, 'Admin'
-        MANAGER = 1, 'Manager'
-        USER = 2, 'User'
+        ADMIN = 0, _('Admin')
+        MANAGER = 1, _('Manager')
+        USER = 2, _('User')
 
     class Status(models.IntegerChoices):
-        CREATED = 0, 'Created'
-        INVITED = 1, 'Invited'
-        ACTIVATED = 2, 'Activated'
-        ACTIVE = 3, 'Active'
-        BLOCKED = 4, 'Blocked'
-        DELETED = 5, 'Deleted'
+        CREATED = 0, _('Created')
+        INVITED = 1, _('Invited')
+        ACTIVATED = 2, _('Activated')
+        ACTIVE = 3, _('Active')
+        BLOCKED = 4, _('Blocked')
+        DELETED = 5, _('Deleted')
 
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
