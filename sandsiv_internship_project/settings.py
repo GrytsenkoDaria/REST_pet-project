cc05fa5b5b406d2af4f67c818949a0ed9a977f58
Daria <daria@example.com>
2020-02-07 17:04:33 +0200

    Provided connection to MySQL database

--- sandsiv_internship_project/settings.py
+++ sandsiv_internship_project/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'sandsiv_internship_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sandsiv_internship_project',
+        'HOST': 'localhost',
+        'PORT': '8000',
+        'USER': 'daria',
+        'PASSWORD': '123',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
