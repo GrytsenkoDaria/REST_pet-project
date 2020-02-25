fe9e86d59bd5951ac1bc876bb0be0c1e16002ea0
Daria <daria@example.com>
2020-02-25 10:59:10 +0200

    Serializers, Views and URLs for Sprints created

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,33 @@
 from django.urls import path
 
-from project.views import ProjectListView, ProjectDetailsView
-from project.views import ReleaseListView, ReleaseDetailsView
+from project.views import ProjectListView, ProjectDetailView
+from project.views import ReleaseListView, ReleaseDetailView
+from project.views import SprintListView, SprintDetailView
+from project.views import SprintProjectListView
 
 urlpatterns = [
     path('', ProjectListView.as_view()),
-    path('<int:pk>/', ProjectDetailsView.as_view()),
+    path('<int:pk>/', ProjectDetailView.as_view()),
     path('<int:project_pk>/releases/', ReleaseListView.as_view()),
-    path('<int:project_pk>/releases/<int:pk>/', ReleaseDetailsView.as_view()),
+
+    path(
+        '<int:project_pk>/releases/<int:pk>/',
+        ReleaseDetailView.as_view()
+    ),
+
+    path(
+        '<int:project_pk>/releases/<int:release_pk>/sprints/',
+        SprintListView.as_view()
+    ),
+
+    path(
+        '<int:project_pk>/releases/<int:release_pk>/sprints/<int:pk>/',
+        SprintDetailView.as_view()
+    ),
+
+    path(
+        '<int:project_pk>/sprints/',
+        SprintProjectListView.as_view()
+    ),
+
 ]
